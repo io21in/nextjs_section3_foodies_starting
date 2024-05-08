1721c95f1c04cc5dcf55b78b569088988306f23f
Hyoungwoo In <io21in@hanmail.net>
2024-05-08 15:15:49 +0900

    lecture_99

--- components/main-header.js
+++ components/main-header.js
@@ -1,15 +1,17 @@
 import Link from "next/link"
 
 import logoImg from '@/assets/logo.png'
+import classes from './main-header.module.css';
 
 export default function MainHeader() {
-    return <header>
-        <Link href="/" >
+    return (
+    <header className={classes.header}>
+        <Link className={classes.logo} href="/" >
             <img src={logoImg.src} alt="A plate with food on it" />
             NextLevel Food
         </Link>
 
-        <nav>
+        <nav className={classes.nav}>
             <ul>
                 <li>
                     <Link href="/meals" >Browse Meals</Link>
@@ -20,4 +22,5 @@ export default function MainHeader() {
             </ul>
         </nav>
     </header>
+    )
 }
